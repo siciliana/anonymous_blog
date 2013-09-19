4504d6b4df584ef3465918503c94f2f088213b5b
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-19 16:26:32 -0500

    worked out post routes

--- app/controllers/posts.rb
+++ app/controllers/posts.rb
@@ -1,5 +1,37 @@
-# Show me all posts
+
+#=========GET==================
+
 # Show me a particular post
-# Create a new post
+
+get '/post' do
+  erb :post
+end 
+
+# Show me all posts
+
+get '/posts' do
+  erb :posts
+end 
+
 # Edit an existing post
+
+get '/post/:id/edit' do
+  @post = Post.find(params[:id])
+  erb :edit_post
+end 
+
 # Delete an existing post
+get '/post/:id/delete' do
+  @post = Post.find(params[:id])
+  @post.destroy
+  redirect '/'
+end 
+
+
+#=========POST==================
+
+# Create a new post
+post '/post' do 
+  #enter stuff here 
+  erb :posts
+end 
--- db/migrate/20130919145518_create_jointables.rb
+++ db/migrate/20130919145518_create_jointables.rb
@@ -1,6 +1,6 @@
 class CreateJointables < ActiveRecord::Migration
   def change
-    create_table :labels do |t|
+    create_join_table :posts, :tags, table_name: :labels do |t|
       t.integer :post_id
       t.integer :tag_id
       t.timestamps 
